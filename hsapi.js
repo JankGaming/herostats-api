cd3f6f14064041ce621e65c93608fcf1ef181020
twisterghost <twisterghost@gmail.com>
2015-07-07 13:34:56 +0000

    Updates to main page copy

--- hsapi.js
+++ hsapi.js
@@ -38,20 +38,21 @@ app.get('/', function(req, res) {
   var welcomeMsg = '<!DOCTYPE html>' +
     '<div style=\'font-family: "Courier New"\'>' +
     'Welcome to the JankDota Hero Stats API!<br />'+
-    'Currently serving stats for patch <strong>' + patchName + '</strong><br /><br />'+
-    'To get JSON of all hero/ID pairs, query http://api.herostats.io/heroes<br /><br />' +
-    'For a single hero, query http://api.herostats.io/heroes/<strong>heroname</strong><br /><br />' +
-    'You can query with the JankDota HeroID with http://api.herostats.io/heroes/<strong>heroID</strong><br /><br />' +
-    '<strong>To get ALL hero data in one query</strong>, use http://api.herostats.io/heroes/all<br /><br />' +
-    '<strong>To get ALL hero data for a given patch</strong>, ' +
-    'use http://api.herostats.io/patch/<strong>patch number</strong><br />' +
-    '<em>Giving an incorrect patch number will return a list of valid patch numbers.' +
+    'Current default patch: <strong>' + patchName + '</strong><br /><br />'+
+    'JSON of all hero/ID pairs: http://api.herostats.io/heroes<br />' +
+    'Single hero data: http://api.herostats.io/heroes/<strong>heroname (or hero ID*)</strong><br />' +
+    'ALL hero data: http://api.herostats.io/heroes/all<br />' +
+    'ALL hero data for past patches: http://api.herostats.io/patch/<strong>patch number</strong><br />' +
+    '<em>Giving an incorrect patch number will return a list of valid patch numbers. ' +
     'HeroStats supports patches back to 6.77.</em><br /><br />' +
     'This API is here to help you easily access Dota 2 hero properties. If you are implementing the API, ' +
     'please use intelligent code for getting the hero data as to not destroy the server with requests! Besides that, ' +
     'this data is free and open to use by anyone.<br /><br />' +
     'See <a target="_blank" href="http://jankdota.com/herostats">jankdota.com/herostats</a> for our in-depth display ' +
-    'of all hero property data.' +
+    'of all hero property data.<br /><br />' +
+    'See our <a href="https://github.com/JankGaming/herostats-api">Github repository</a><br /><br />' +
+    'Version 2 is in development with hero leveling, filtering options and more.<br /><br />' +
+    '<em>*JankDota hero ID from /heroes, not Valve API hero id</em>' +
     '</div>';
   res.send(welcomeMsg);
   res.end();
